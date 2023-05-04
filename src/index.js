3097a7639dd2d3ad8fafb801d30b32d36675b8b1
João Antonio - Ikidon Dev <ikidon.dev@gmail.com>
2023-05-04 02:02:44 -0300

    challenge concluded!

--- src/index.js
+++ src/index.js
@@ -1,41 +1,145 @@
-const express = require('express');
-const cors = require('cors');
+const { v4: uuidv4 } = require("uuid");
 
-// const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response
+      .status(404)
+      .json({ error: "There is no user with that username" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post("/users", (request, response) => {
+  const { name, username } = request.body;
+
+  const userAlreadyExists = users.some((user) => user.username === username);
+
+  if (userAlreadyExists) {
+    return response
+      .status(400)
+      .json({ error: "A user with that username already exists." });
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  };
+
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.get("/todos", checksExistsUserAccount, (request, response) => {
+  return response.json(request.user.todos);
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.post("/todos", checksExistsUserAccount, (request, response) => {
+  const { title, deadline } = request.body;
+
+  const task = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  const userIndex = users.findIndex(
+    (user) => user.username === request.user.username
+  );
+  users[userIndex].todos.push(task);
+
+  return response.status(201).json(task);
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+
+  const userIndex = users.findIndex(
+    (user) => user.username === request.user.username
+  );
+
+  const taskIndex = request.user.todos.findIndex((task) => task.id === id);
+
+  if (taskIndex == -1) {
+    return response.status(404).json({ error: "This task does not exist" });
+  }
+
+  const task = request.user.todos[taskIndex];
+
+  const updatedTask = {
+    ...task,
+    title,
+    deadline,
+  };
+
+  users[userIndex].todos[taskIndex] = updatedTask;
+
+  return response.json(updatedTask);
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
+  const { id } = request.params;
+
+  const userIndex = users.findIndex(
+    (user) => user.username === request.user.username
+  );
+
+  const taskIndex = request.user.todos.findIndex((task) => task.id === id);
+
+  if (taskIndex == -1) {
+    return response.status(404).json({ error: "This task does not exist" });
+  }
+
+  const task = request.user.todos[taskIndex];
+
+  const updatedTask = {
+    ...task,
+    done: true,
+  };
+
+  users[userIndex].todos[taskIndex] = updatedTask;
+
+  return response.json(updatedTask);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
+  const { id } = request.params;
+
+  const userIndex = users.findIndex(
+    (user) => user.username === request.user.username
+  );
+
+  const taskIndex = request.user.todos.findIndex((task) => task.id === id);
+
+  if (taskIndex == -1) {
+    return response.status(404).json({ error: "This task does not exist" });
+  }
+
+  users[userIndex].todos = request.user.todos.splice(taskIndex, -1);
+
+  return response.status(204).json();
 });
 
 module.exports = app;
